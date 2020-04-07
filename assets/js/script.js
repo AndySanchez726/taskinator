eaf7cb3bcf92f6921e721952959bef13d9ccac55
Andy Sanchez <andysanchez726@gmail.com>
2020-04-07 11:57:54 -0500

    add new task

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,13 +5,21 @@ var createTaskHandler = function (event) {
 
     event.preventDefault();
 
+    var taskNameInput = document.querySelector("input[name='task-name']").value;
+    var taskTypeInput = document.querySelector("select[name='task-type']").value;
+
+    //create list item
     var taskItemEl = document.createElement("li");
     taskItemEl.className = "task-item";
-    taskItemEl.textContent = window.prompt("New Task");
-    if(taskItemEl.textContent) {
-    } else {
-        return;
-    }
+    
+    //create div to hold task info and add to list item
+    var taskInfoEl = document.createElement("div");
+    //give it a class name
+    taskInfoEl.className = "task-info";
+    //add HTML content to div
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+    taskItemEl.appendChild(taskInfoEl);
+     // add entire list item to list
     tasksToDoEl.appendChild(taskItemEl);
 }
 
