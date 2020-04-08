c4a9be52e7bd10008f5a08857801fcbf1090c3b1
Andy Sanchez <andysanchez726@gmail.com>
2020-04-08 14:58:13 -0500

    delete button working

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
 var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 var taskIdCounter = 0;
+var pageContentEl = document.querySelector("#page-content");
 
 var createFormHandler = function (event) {
     event.preventDefault();
@@ -23,6 +24,7 @@ var createFormHandler = function (event) {
     //send it as an argument to createTaskEl
     createTaskEl(taskDataObj);
 };
+
 var createTaskEl = function(taskDataObj) {
     //create list item
     var taskItemEl = document.createElement("li");
@@ -86,4 +88,18 @@ var createTaskAction = function(taskIdCounter) {
     return actionContainerEl
 };
 
+var taskButtonHandler = function(event) {
+        console.log(event.target);
+
+    if(event.target.matches(".delete-btn")) {
+        var taskId = event.target.getAttribute("data-task-id");
+        deleteTask(taskId);
+    }
+};
+
+var deleteTask = function(taskId) {
+    var taskSelected = document.querySelector(".task-item[data-task-id='" + taskId + "']");
+    taskSelected.remove();
+}
 formEl.addEventListener("submit", createFormHandler)
+pageContentEl.addEventListener("click", taskButtonHandler)
